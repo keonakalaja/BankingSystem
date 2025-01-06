c845e9a6a9dc28578dc628b0a08c6935f9fe2a66
keonakalaja <kalajakeona05@gmail.com>
2025-01-06 10:32:01 +0100

    Changed Main file

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Account account1 = new Account(1000);
-        Account account2 = new Account();
-
-        System.out.println("Account 1 Balance: " + account1.checkBalance());
-        System.out.println("Account 2 Balance: " + account2.checkBalance());
-
-        account1.transferMoney(account2, 300);
-
-        System.out.println("After Transfer:");
-        System.out.println("Account 1 Balance: " + account1.checkBalance());
-        System.out.println("Account 2 Balance: " + account2.checkBalance());
+        
     }
 }
